3925f335943364bb20f586f26eb6e9cb9f845787
Alexandr Topilski <atopilski@gmail.com>
2019-06-23 10:20:19 +0300

    Update public_config.py

--- app/config/public_config.py
+++ app/config/public_config.py
@@ -4,7 +4,7 @@ PUBLIC_CONFIG = {'site': {'title': 'FastoTV', 'keywords': 'video,cloud,iptv,play
                  'company': {'title': 'FastoGT', 'domain': 'https://fastogt.com'},
                  'support': {'contact_email': 'support@fastogt.com',
                              'contact_address': 'Republic of Belarus, Minsk, Stadionnay str. 5'},
-                 'project': {'version': '1.0.0', 'version_type': 'release',
+                 'project': {'version': '1.1.0', 'version_type': 'release',
                              'github': {'issues': 'https://github.com/fastogt/fastotv/issues',
                                         'repo': 'https://github.com/fastogt/fastotv'}
                              },
